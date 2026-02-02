5467b1963c5256041f8a6ccb634ff1d15a3704b4
KeymalW <keymalwijaya@gmail.com>
2026-02-02 11:34:07 +0700

    feat(aspirations): setup Aspirasi model with user relationship

---
+++ app/Models/Aspirasi.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Aspirasi extends Model
+{
+    protected $primaryKey = 'id_aspirasi';
+
+    protected $fillable = [
+        'status',
+        'id_kategori',
+        'feedback',
+        'id_pelaporan',
+    ];
+
+    public function inputAspirasi()
+    {
+        return $this->belongsTo(InputAspirasi::class, 'id_pelaporan', 'id_pelaporan');
+    }
+
+    public function kategori()
+    {
+        return $this->belongsTo(Kategori::class, 'id_kategori', 'id_kategori');
+    }
+}
