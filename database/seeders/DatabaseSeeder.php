0983bab31ac9d2e9c1d2814279b31acfb97fa87e
KeymalW <keymalwijaya@gmail.com>
2026-02-07 14:01:31 +0700

    chore: update seeder with initial user accounts and categories

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,24 +2,43 @@
 
 namespace Database\Seeders;
 
-use App\Models\User;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Admin;
+use App\Models\Kategori;
+use App\Models\Siswa;
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
-    use WithoutModelEvents;
-
     /**
      * Seed the application's database.
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        // Admin
+        Admin::create([
+            'username' => 'admin',
+            'password' => Hash::make('admin123'),
         ]);
+
+        // Siswas
+        Siswa::create([
+            'nis' => '1234567890',
+            'kelas' => 'XII RPL 1',
+            'password' => Hash::make('siswa123'),
+        ]);
+
+        Siswa::create([
+            'nis' => '0987654321',
+            'kelas' => 'XII RPL 2',
+            'password' => Hash::make('siswa123'),
+        ]);
+
+        // Kategoris
+        Kategori::create(['ket_kategori' => 'Kebersihan']);
+        Kategori::create(['ket_kategori' => 'Kerusakan']);
+        Kategori::create(['ket_kategori' => 'Fasilitas Kelas']);
+        Kategori::create(['ket_kategori' => 'Fasilitas Olahraga']);
     }
 }
